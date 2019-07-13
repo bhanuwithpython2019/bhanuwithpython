b5bce2e4dbcb34ff40d98b4996a1266d3db83580
bhanuwithpython2019 <52854791+bhanuwithpython2019@users.noreply.github.com>
2019-07-13 20:03:42 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,28 @@
+# datatypes in python
+a=10 # int
+b=10.5 # float
+c=2+3j # complex
+d=True # boolean
+e="swetha" # string
+f= [1,2,3,4,5] # list
+g= (1,2,3,4,5) # tuple
+h={1,2,3,3,4,5,6} # set
+i= {'a': 10, 'b': 20} # dictionary
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
+print(i)
+print(type(i))
